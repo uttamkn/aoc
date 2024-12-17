213ad17353a8d314c92fc3b3245575921ae91e7c
uttamkn <uttamkn15@gmail.com>
2024-12-17 20:37:10 +0530

    stack overflow for part1 (have to use dijkstra's or A* with heuristic)

--- 24/day16.py
+++ 24/day16.py
@@ -1,3 +1,7 @@
+import math
+import sys
+sys.setrecursionlimit(10**6)
+
 def get_input():
     maze = []
     start_pos = []
@@ -16,16 +20,57 @@ def get_input():
     return maze, start_pos
 
 
-def backtrack(maze, visited, cur_pos, cur_score, min_score, dirs, cur_dir):
+def backtrack(maze, visited, cur_pos, cur_score, dirs, cur_dir, memo):
     x, y = cur_pos
-    if maze[x][y] == "#":
-        return min_score 
+    if maze[x][y] == "#" or visited[x][y]:
+        return math.inf
+
+    if maze[x][y] == "E":
+        return cur_score
+
+    if (x, y, cur_dir, cur_score) in memo:
+        return memo[(x, y, cur_dir, cur_score)]
+
+    visited[x][y] = 1
+
+    right = (4 + cur_dir + 1) % 4
+    left = (4 + cur_dir - 1) % 4
+
+    x_dir = dirs[cur_dir][0]
+    y_dir = dirs[cur_dir][1]
+    # straight
+    straight_score = backtrack(
+        maze, visited, [x + x_dir, y + y_dir], cur_score + 1, dirs, cur_dir, memo
+    )
+
+    x_dir = dirs[right][0]
+    y_dir = dirs[right][1]
+    # right
+    right_score = backtrack(
+        maze, visited, [x + x_dir, y + y_dir], cur_score + 1001, dirs, right, memo
+    )
+
+    x_dir = dirs[left][0]
+    y_dir = dirs[left][1]
+    # left
+    left_score = backtrack(
+        maze, visited, [x + x_dir, y + y_dir], cur_score + 1001, dirs, left, memo
+    )
+
+    visited[x][y] = 0
+
+    memo[(x, y, cur_dir, cur_score)] = min(straight_score, right_score, left_score)
+    return memo[(x, y, cur_dir, cur_score)]
 
 
 def solve(maze, start_pos):
     dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-    visited = [[0 for _ in range(len(maze))] for _ in range(len(maze))]
+    visited = [[0 for _ in range(len(maze[0]))] for _ in range(len(maze))]
+    memo ={}
+
+    return backtrack(maze, visited, start_pos, 0, dirs, 1, memo)
 
 
 if __name__ == "__main__":
-    print(get_input())
+    maze, start_pos = get_input()
+    print(solve(maze, start_pos))
